d002eb39b437224e70fa9a9053b733877d717f46
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-12 11:09:47 -0300

    1 file modified

--- src/app/price-btc/price-btc.component.ts
+++ src/app/price-btc/price-btc.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BitcoinService } from '../bitcoin.service';
 
 @Component({
   selector: 'app-price-btc',
   templateUrl: './price-btc.component.html',
-  styleUrls: ['./price-btc.component.css']
+  styleUrls: ['./price-btc.component.css'],
 })
 export class PriceBtcComponent implements OnInit {
-
-  constructor() { }
+  constructor(public bitcoinService: BitcoinService) {}
 
   ngOnInit() {
+    this.update();
   }
 
+  getCurrentPrice() {
+    return this.bitcoinService.currentPrice;
+  }
+
+  update() {
+    this.bitcoinService.update();
+  }
 }
