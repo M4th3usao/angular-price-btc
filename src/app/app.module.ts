3c6324585c74ca45d57af5920b01150298c3ae3f
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-12 11:06:25 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PriceBtcComponent } from './price-btc/price-btc.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    PriceBtcComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/price-btc/price-btc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-btc',
+  templateUrl: './price-btc.component.html',
+  styleUrls: ['./price-btc.component.css']
+})
+export class PriceBtcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
