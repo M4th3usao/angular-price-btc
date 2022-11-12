dd98f33f861e51f6e11eeb0a12e21630ba153d33
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-12 10:30:49 -0300

    4 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  name: string = 'Matheus Henrique Cruz de Oliveira';
+  RA: string = '0050832021009';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
