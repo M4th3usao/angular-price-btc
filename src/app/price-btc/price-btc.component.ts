14a702360d176eda916039fd1c0ce4f38c010ce1
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-15 14:08:35 -0300

    2 files modified

--- src/app/price-btc/price-btc.component.ts
+++ src/app/price-btc/price-btc.component.ts
@@ -9,10 +9,6 @@ import { BitcoinService } from '../bitcoin.service';
 export class PriceBtcComponent implements OnInit {
   constructor(public bitcoinService: BitcoinService) {}
 
-  // ws = new WebSocket('wss://stream.binance.com:9443/ws/btc@trade');
-  // stockPriceElement = document.getElementById('stock-price');
-  // lastPrice = null;
-
   ngOnInit() {
     this.update();
   }
@@ -24,12 +20,4 @@ export class PriceBtcComponent implements OnInit {
   update() {
     this.bitcoinService.update();
   }
-
-  // onmessage = (event) => {
-  //   let stockObject = JSON.parse(event.data);
-  //   let price = parseFloat(stockObject.p).toFixed(2);
-  //   this.stockPriceElement.innerText = price;
-  //   this.stockPriceElement.style.color = !this.lastPrice || this.lastPrice === price ? 'black' : price > this.lastPrice ? 'green' : 'red';
-  //   this.lastPrice = price;
-  // }
 }
